76d89e51d9ee202f582ce4b3455cb1868401ee76
Javier Taccone <javiertaccone@gmail.com>
2024-07-10 22:15:14 +0200

    Creación de User y Rol

---
+++ appweb/src/main/java/org/proyect/appweb/entities/Rol.java
@@ -0,0 +1,25 @@
+package org.proyect.appweb.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Roles")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Rol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "rol usuario", nullable = false, unique = true)
+    private Rolename rol_usuario;
+}
+
---
+++ appweb/src/main/java/org/proyect/appweb/entities/User.java
@@ -0,0 +1,51 @@
+package org.proyect.appweb.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+
+@Entity
+@Table (name = "Users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (name = "name")
+    private String name;
+
+    @Column (name = "username")
+    private String username;
+
+    @Column (name = "email")
+    private String email;
+
+    @Column (name = "surname")
+    private String surname;
+
+    @Column (name = "password")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "rol usuario", nullable = false)
+    private Rolename rol_usuario;
+
+    @Column (name = "birthDate")
+    private Date birthDate;
+
+    @Column (name = "lastLoginAt")
+    private LocalDate lastLoginAt;
+
+    @Column (name = "createdAt")
+    private LocalDate createdAt;
+
+}
